da0cdb2870604296c2fceda4198d7caf3d787156
Gab-P12 <gabrielapm.002@gmail.com>
2025-09-07 00:49:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -67,24 +67,3 @@ document.addEventListener("DOMContentLoaded", function () {
         modal.classList.add("hidden");
     });
 });
-
-const form = document.getElementById("contactForm");
-const msgStatus = document.getElementById("msgStatus");
-
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  
-  const data = new FormData(form);
-  const response = await fetch(form.action, {
-    method: form.method,
-    body: data,
-    headers: { 'Accept': 'application/json' }
-  });
-
-  if (response.ok) {
-    msgStatus.innerText = "✅ Mensagem enviada com sucesso!";
-    form.reset();
-  } else {
-    msgStatus.innerText = "❌ Ocorreu um erro. Tente novamente.";
-  }
-});
